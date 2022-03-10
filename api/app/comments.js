040459ba0758701469d6796309ffdd9cea016b62
Tugolbai Maksatbek uulu <mkstbkv751@gmail.com>
2022-03-10 22:07:12 +0600

    роуты для полтзователей, постов, комментариев

---
+++ api/app/comments.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const mongoose = require("mongoose");
+const Comment = require("../models/Comment")
+const auth = require("../middleware/auth");
+
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+    try {
+        const comments = await Comment.find().sort({_id: -1});
+
+        return res.send(comments);
+    } catch (e) {
+        next(e);
+    }
+});
+
+router.post('/', auth,  async (req, res, next) => {
+    try {
+        if (!req.body.text || !req.body.post) {
+            return res.status(400).send({message: 'Text and postID are required'});
+        }
+
+        const commentData = {
+            text: req.body.text,
+            post: req.body.post,
+            user: req.user._id
+        };
+
+        const comment = new Comment(commentData);
+
+        await comment.save();
+
+        return res.send({message: 'Created new comment', id: comment._id});
+    } catch (e) {
+        if (e instanceof mongoose.Error.ValidationError) {
+            return res.status(400).send(e);
+        }
+
+        next(e);
+    }
+});
+
+module.exports = router;
---
+++ api/app/posts.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const mongoose = require("mongoose");
+const Post = require("../models/Post");
+const auth = require("../middleware/auth");
+const path = require('path');
+const multer = require('multer');
+const { nanoid } = require('nanoid');
+const config = require('../config');
+
+const router = express.Router();
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, config.uploadPath);
+    },
+    filename: (req, file, cb) => {
+        cb(null, nanoid() + path.extname(file.originalname))
+    }
+});
+
+const upload = multer({storage});
+
+router.get('/', async (req, res, next) => {
+    try {
+        const posts = await Post.find().sort({_id: -1}).populate("user", "displayName");
+
+        return res.send(posts);
+    } catch (e) {
+        next(e);
+    }
+});
+
+router.post('/', auth, upload.single('image'), async (req, res, next) => {
+    try {
+        if (!req.body.title) {
+            return res.status(400).send({message: 'Title is required'});
+        }
+
+        const postData = {
+            title: req.body.title,
+            description: null,
+            image: null,
+            user: req.user._id
+        };
+
+        if (req.body.description) {
+            postData.description = req.body.description;
+        }
+
+        if (req.file) {
+            postData.image = req.file.filename;
+        }
+
+        const post = new Post(postData);
+
+        await post.save();
+
+        return res.send({message: 'Created new post', id: post._id});
+    } catch (e) {
+        if (e instanceof mongoose.Error.ValidationError) {
+            return res.status(400).send(e);
+        }
+
+        next(e);
+    }
+});
+
+module.exports = router;
